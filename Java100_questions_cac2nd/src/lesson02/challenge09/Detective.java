4cc7bb108b724fa07108c9f7ce1ff11ff97c9649
miurama <miurama@C010423593.zipangu.cac.co.jp>
2026-04-15 11:06:45 +0900

    lesson02 完了

--- Java100_questions_cac2nd/src/lesson02/challenge09/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge09/Detective.java
@@ -67,7 +67,11 @@ public class Detective {
 		int weight = Integer.parseInt(weightStr);
 
 		//ここに記述
-
+		if ((190 <= height) && (weight <= 70)) {
+			System.out.println("探偵：\nその方は犯人である可能性がありますね。");
+		} else {
+			System.out.println("探偵：\nその方は犯人ではありませんね。");
+		}
 	}
 
 }
